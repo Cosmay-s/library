6ede77fcef99ad9a465a715e3726f2765494fce5
Sergey Simakov <SaneniRem@yandex.ru>
2025-05-17 23:19:25 +0300

    models

--- library_api/schemas.py
+++ library_api/schemas.py
@@ -2,12 +2,11 @@ from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 
-# Схема для пользователя
 class User(BaseModel):
     email: EmailStr
     password: str
 
-# Схема для книги
+
 class Book(BaseModel):
     title: str
     author: str
@@ -15,12 +14,12 @@ class Book(BaseModel):
     isbn: Optional[str] = None
     copies_count: int
 
-# Схема для читателя
+
 class ReaderCreate(BaseModel):
     name: str
     email: EmailStr
 
-# Схема для выдачи книги
+
 class BorrowBook(BaseModel):
     book_id: int
     reader_id: int
