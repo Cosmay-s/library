e4d11555ce28b7e4eed0f6e11b04bd36aef9d2c8
Sergey Simakov <SaneniRem@yandex.ru>
2025-05-18 18:41:50 +0300

    Переделка под sqlite

--- library_api/main.py
+++ library_api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from library_api.routers import book, borrow, reader, users
+from library_api.routers import book, borrow, reader, user
 
 app = FastAPI()
 
 # Регистрация роутов
-app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(user.router, prefix="/users", tags=["users"])
 app.include_router(book.router, prefix="/books", tags=["books"])
 app.include_router(reader.router, prefix="/readers", tags=["readers"])
 app.include_router(borrow.router, prefix="/borrow", tags=["borrow"])
--- library_api/models.py
+++ library_api/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime, UTC
 from typing import Optional, List
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
-from sqlalchemy import String, ForeignKey, UniqueConstraint
+from sqlalchemy import String, ForeignKey, UniqueConstraint, Integer
 
 
 class Base(DeclarativeBase):
@@ -12,12 +12,11 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String, unique=True, index=True,
-                                       nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC),
-                                                 onupdate=datetime.now(tz=UTC))
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC))
+    updated_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC),
+                                                 onupdate=lambda: datetime.now(tz=UTC))
 
 
 class Book(Base):
@@ -26,15 +25,14 @@ class Book(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(nullable=False)
     author: Mapped[str] = mapped_column(nullable=False)
-    year_published: Mapped[Optional[int]]
+    year_published: Mapped[Optional[int]] = mapped_column(Integer)
     isbn: Mapped[Optional[str]] = mapped_column(String, unique=True)
     copies_count: Mapped[int] = mapped_column(default=1, nullable=False)
     description: Mapped[Optional[str]] = mapped_column(default=None)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC),
-                                                 onupdate=datetime.now(tz=UTC))
-    borrowed_books: Mapped[List["BorrowedBook"]] = relationship(
-                                                    back_populates="book")
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC))
+    updated_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC),
+                                                 onupdate=lambda: datetime.now(tz=UTC))
+    borrowed_books: Mapped[List["BorrowedBook"]] = relationship(back_populates="book")
 
 
 class Reader(Base):
@@ -43,27 +41,24 @@ class Reader(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
-    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC),
-                                                 onupdate=datetime.now(tz=UTC))
-    borrowed_books: Mapped[List["BorrowedBook"]] = relationship(
-                                                    back_populates="reader")
+    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC))
+    updated_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC),
+                                                 onupdate=lambda: datetime.now(tz=UTC))
+    borrowed_books: Mapped[List["BorrowedBook"]] = relationship(back_populates="reader")
 
 
 class BorrowedBook(Base):
     __tablename__ = "borrowed_books"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"),
-                                         nullable=False)
-    reader_id: Mapped[int] = mapped_column(ForeignKey("readers.id"),
-                                           nullable=False)
-    borrow_date: Mapped[datetime] = mapped_column(default=datetime.now(tz=UTC))
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"), nullable=False)
+    reader_id: Mapped[int] = mapped_column(ForeignKey("readers.id"), nullable=False)
+    borrow_date: Mapped[datetime] = mapped_column(default=lambda: datetime.now(tz=UTC))
     return_date: Mapped[Optional[datetime]]
+
     book: Mapped["Book"] = relationship(back_populates="borrowed_books")
     reader: Mapped["Reader"] = relationship(back_populates="borrowed_books")
 
     __table_args__ = (
-        UniqueConstraint("book_id", "reader_id", "return_date",
-                         name="uq_borrow_unique_active"),
+        UniqueConstraint("book_id", "reader_id", "return_date", name="uq_borrow_unique_active"),
     )
